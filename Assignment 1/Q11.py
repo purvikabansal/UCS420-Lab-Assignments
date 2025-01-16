8f4650a83ca0424270765a025694d44108b2244a
purvikabansal <pbansal2_be23@thapar.edu>
2025-01-16 10:50:31 +0530

    Create Q11.py

---
+++ Assignment 1/Q11.py
@@ -0,0 +1,15 @@
+#11.1
+import math
+num = float(input("Enter the no."))
+print( math.sqrt(num))
+
+#11.2
+from datetime import datetime
+print("Current date and time ",datetime.now())
+
+#11.3
+import os
+files=os.listdir(".")
+print("Files in the current direc are",files)
+
+
