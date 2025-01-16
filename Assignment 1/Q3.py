040ffa42150b5f95a3480359bff8f686038491ad
purvikabansal <pbansal2_be23@thapar.edu>
2025-01-16 09:56:59 +0530

    Q3.py

---
+++ Assignment 1/Q3.py
@@ -0,0 +1,17 @@
+#3.1
+number = float(input("Enter a num"))
+
+if number>0:
+    print("A positive number")
+elif number<0:
+    print("A negative number")
+else:
+    print("Number is zero")
+
+#3.2
+num=int(input("Enter the num"))
+
+if num%2==0
+   print("Number is even")
+else
+   print("Number is odd")
